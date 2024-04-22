a8e2121ab419f64ed0cb110f78e24d6dac65f2ac
D1_X_0N <Dimaxiton5@yandex.ru>
2024-04-22 03:02:16 +0300

    base work

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,49 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+console.log("Let's rock");
+console.log(encoded, translations);
 
+let decoding_ignore = {
+  groupId: true,
+  service: true,
+  formatSize: true,
+  ca: true,
+};
 
+let unique_ids = {};
+let decoded = [];
 
-// console.log(decoded)
+function decoding() {
+  for (let groupNum in encoded) {
+    let decoded_group = {};
+
+    for (let key in encoded[groupNum]) {
+      if (decoding_ignore[key] || encoded[groupNum][key] === null) {
+        decoded_group[key] = encoded[groupNum][key];
+        continue;
+      }
+
+      if (key.includes("Id")) {
+        if (unique_ids[key] === undefined) {
+          unique_ids[key] = new Set();
+        } else {
+          unique_ids[key].add(encoded[groupNum][key]);
+        }
+
+        if (translations[key] === undefined) {
+          decoded_group[key] = encoded[groupNum][key];
+        } else {
+          decoded_group[key] = translations[encoded[groupNum][key]];
+        }
+      }
+    }
+
+    decoded.push(decoded_group);
+    decoded_group = {};
+  }
+}
+
+decoding();
+
+console.log(decoded);
+console.log(unique_ids);
--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,101 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+
+  let currency1: number = 0;
+  let currency2: number = 0;
+  let currenciesRates: any;
+  let nameCurrencies1: string = "RUB";
+  let nameCurrencies2: string = "USD";
+
+  onMount(() => {
+    getExchangeRate();
+  });
+
+  function convertCurrency1ToCurrency2() {
+    if (currency1 < 0) {
+      currency1 = 0;
+    }
+
+    currency2 =
+      currency1 *
+      (currenciesRates[nameCurrencies2] / currenciesRates[nameCurrencies1]);
+  }
+
+  function convertCurrency2ToCurrency1() {
+    if (currency2 < 0) {
+      currency2 = 0;
+    }
+
+    currency1 =
+      currency2 *
+      (currenciesRates[nameCurrencies1] / currenciesRates[nameCurrencies2]);
+  }
+
+  async function getExchangeRate() {
+    let response: Response = await fetch(
+      "https://open.er-api.com/v6/latest/USD"
+    );
+
+    if (response.ok) {
+      let json = await response.json();
+      currenciesRates = json.rates;
+    } else {
+      console.log("err: ", response.status);
+    }
+  }
+
+  function handleCurrencyChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+
+    if (target.id.at(-1) === "1") {
+      nameCurrencies1 = target.value;
+      convertCurrency1ToCurrency2();
+    } else {
+      nameCurrencies2 = target.value;
+      convertCurrency2ToCurrency1();
+    }
+  }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<div>
+  <a href="https://www.exchangerate-api.com">Курсы по API обменного курса</a>
+  <br />
+  <label for="currency-select">Выберите валюту 1:</label>
+  <select
+    id="currency-select1"
+    bind:value={nameCurrencies1}
+    on:change={handleCurrencyChange}
+  >
+    <option value="RUB" selected>RUB</option>
+    <option value="TRY">TRY</option>
+    <option value="USD">USD</option>
+  </select>
+  <input
+    type="number"
+    bind:value={currency1}
+    on:input={convertCurrency1ToCurrency2}
+  />
+  <br />
+  <label for="currency-select">Выберите валюту 2:</label>
+  <select
+    id="currency-select2"
+    bind:value={nameCurrencies2}
+    on:change={handleCurrencyChange}
+  >
+    <option value="RUB">RUB</option>
+    <option value="TRY">TRY</option>
+    <option value="USD" selected>USD</option>
+  </select>
+  <input
+    type="number"
+    bind:value={currency2}
+    on:input={convertCurrency2ToCurrency1}
+  />
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  input {
+    margin: 5px;
+    padding: 5px;
   }
 </style>
